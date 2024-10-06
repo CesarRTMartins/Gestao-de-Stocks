d1c19d05599fb7e347488933a4260a6df6dc1e35
Cesar <GFGexample@gmail.orgg>
2024-10-06 17:09:46 +0100

    2(Modelos e Tabelas)

--- pap/stocks/models.py
+++ pap/stocks/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# Modelos
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)  # Nome do produto
+    descricao = models.TextField(null=True, blank=True)  # Descrição do produto
+    preco = models.DecimalField(max_digits=10, decimal_places=2)  # Preço do produto
+    quantidade = models.IntegerField()  # Quantidade em stock
+    data_criacao = models.DateTimeField(auto_now_add=True)  # Data de criação do produto
+    data_atualizacao = models.DateTimeField(auto_now=True)  # Data de atualização do produto
+
+    class Meta:
+        db_table = 'produtos'  # Define o nome da tabela no banco de dados
+
+    def __str__(self):
+        return self.nome
