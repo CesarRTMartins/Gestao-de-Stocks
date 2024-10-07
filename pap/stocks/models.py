ded96a890bdb7344ce05fbb259adedf94271f9c7
Cesar <GFGexample@gmail.orgg>
2024-10-07 18:18:30 +0100

    Terceira

--- pap/stocks/models.py
+++ pap/stocks/models.py
@@ -3,15 +3,15 @@ from django.db import models
 # Modelos
 
 class Produto(models.Model):
-    nome = models.CharField(max_length=100)  # Nome do produto
-    descricao = models.TextField(null=True, blank=True)  # Descrição do produto
-    preco = models.DecimalField(max_digits=10, decimal_places=2)  # Preço do produto
-    quantidade = models.IntegerField()  # Quantidade em stock
-    data_criacao = models.DateTimeField(auto_now_add=True)  # Data de criação do produto
-    data_atualizacao = models.DateTimeField(auto_now=True)  # Data de atualização do produto
+        nome = models.CharField(max_length=100)  # Nome do produto
+        descricao = models.TextField(null=True, blank=True)  # Descrição do produto
+        preco = models.DecimalField(max_digits=10, decimal_places=2)  # Preço do produto
+        quantidade = models.IntegerField()  # Quantidade em stock
+        data_criacao = models.DateTimeField(auto_now_add=True)  # Data de criação do produto
+        data_atualizacao = models.DateTimeField(auto_now=True)  # Data de atualização do produto
 
-    class Meta:
-        db_table = 'produtos'  # Define o nome da tabela no banco de dados
+        class Meta:
+            db_table = 'produtos'  # Define o nome da tabela no banco de dados
 
-    def __str__(self):
-        return self.nome
+        def __str__(self):
+            return self.nome
