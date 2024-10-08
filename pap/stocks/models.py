1b6a8357c7c16500b27db66753e3ec198170619f
Cesar <GFGexample@gmail.orgg>
2024-10-08 09:06:51 +0100

    Quinta(Tabela users)

--- pap/stocks/models.py
+++ pap/stocks/models.py
@@ -16,3 +16,15 @@ class Products(models.Model):
         def __str__(self):
             return self.name
 
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)  # Hashed password
+    position = models.CharField(max_length=50)  # Ex.: administrador, operador
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'users'
