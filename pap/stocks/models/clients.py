9d904036734b56e083b08bf3e1b06fa124ff8132
Cesar <cesarrtmartins@gmail.com>
2024-11-25 16:03:08 +0000

    Criação de tabelas(ClientAdress)

--- pap/stocks/models/clients.py
+++ pap/stocks/models/clients.py
@@ -32,8 +32,24 @@ class Client(models.Model):
     contact = models.CharField(max_length=200) # Contacto
     email = models.CharField(max_length=200) # Email
     password = models.CharField(max_length=200) # Password
-    creation_date = models.DateTimeField(auto_now_add=True) # Data de criação da conta do Administrator
-    update_date = models.DateTimeField(auto_now=True) # Data de atualização da conta do Administrator
+    creation_date = models.DateTimeField(auto_now_add=True) # Data de criação
+    update_date = models.DateTimeField(auto_now=True) # Data de atualização 
 
     class Meta:
         db_table = 'client'  # Definindo o nome personalizado da tabela
+
+class ClientAddress(models.Model):
+    # Chave Estrangeira
+    client = models.ForeignKey(Client,on_delete=models.CASCADE,db_column='client_id')
+    country = models.ForeignKey(Countries,on_delete=models.CASCADE,db_column='country_id') 
+    # Atributos da tabela
+    active = models.PositiveSmallIntegerField(choices=[(0, 'Inactive'), (1, 'Active')], default=1)  # Activo ou não
+    address = models.CharField(max_length=200) # Morada 
+    postalcode = models.CharField(max_length=20) # Código Postal
+    locality = models.CharField(max_length=200) # Morada 
+    city = models.CharField(max_length=200) # Cidade
+    creation_date = models.DateTimeField(auto_now_add=True) # Data de criação 
+    update_date = models.DateTimeField(auto_now=True) # Data de atualização 
+
+    class Meta:
+        db_table = 'client_address'  # Definindo o nome personalizado da tabela
